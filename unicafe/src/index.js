1e92866ef9d3d7e2c1041b78d2a450cb6b70a4eb
Inti <intirocks888@gmail.com>
2021-07-03 16:09:49 -0600

    exercise 1.8

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -4,11 +4,34 @@ import ReactDOM from 'react-dom'
 
 
 
-const Button =(props)=>(
+const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 const Display = props => <div>{props.value}</div>
 
+const Statistics = (props) => {
+  if (props.all !== 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <div>
+          <h2>good</h2><Display value={props.good}></Display>
+          <h2>neutral</h2><Display value={props.neutral}></Display>
+          <h2>bad</h2><Display value={props.bad}></Display>
+          <h2>all</h2><Display value={props.all}></Display>
+          <h2>average</h2><Display value={props.average}></Display>
+          <h2>positive</h2><Display value={props.positivePercent + "%"}></Display>
+        </div>
+      </div>
+    )
+  }
+  return(
+    <div>
+      <h1>No feedback given yet</h1>
+    </div>
+  )
+
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -16,49 +39,39 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
 
-  const addGood=nuevoValor =>{
+  const addGood = nuevoValor => {
     setGood(nuevoValor)
-    setAll(all+1)
+    setAll(all + 1)
   }
-  const addBad=nuevoValor =>{
+  const addBad = nuevoValor => {
     setBad(nuevoValor)
-    setAll(all+1)
+    setAll(all + 1)
   }
-  const addNeutral=nuevoValor =>{
+  const addNeutral = nuevoValor => {
     setNeutral(nuevoValor)
-    setAll(all+1)
+    setAll(all + 1)
   }
-  const average=()=>{
+  const average = () => {
     let positive = good
-    let negative = bad*-1
-    return (positive+negative)/all
+    let negative = bad * -1
+    return (positive + negative) / all
   }
-  const positivePercent=()=>{
-    return (good/all)*100
+  const positivePercent = () => {
+    return (good / all) * 100
   }
   return (
     <div>
       <h1>Give feedback</h1>
       <div>
-        <Button handleClick={()=>addGood(good+1)} text={"good"}></Button>
-        <Button handleClick={()=>addNeutral(neutral+1)} text={"neutral"}></Button>
-        <Button handleClick={()=>addBad(bad+1)} text={"bad"}></Button>
+        <Button handleClick={() => addGood(good + 1)} text={"good"}></Button>
+        <Button handleClick={() => addNeutral(neutral + 1)} text={"neutral"}></Button>
+        <Button handleClick={() => addBad(bad + 1)} text={"bad"}></Button>
       </div>
-      <div>
-        <h1>Statistics</h1>
-        <div>
-          <h2>good</h2><Display value={good}></Display>
-          <h2>neutral</h2><Display value={neutral}></Display>
-          <h2>bad</h2><Display value={bad}></Display>
-          <h2>all</h2><Display value={all}></Display>
-          <h2>average</h2><Display value={average()}></Display>
-          <h2>positive</h2><Display value={positivePercent()+"%"}></Display>
-        </div>
-      </div>
+    <Statistics good={good} bad={bad} all={all} neutral={neutral} average={average()} positivePercent={positivePercent()}></Statistics>
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
