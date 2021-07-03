6902d0057566bee8b5df8d7a7b7d228f2721a246
Inti <intirocks888@gmail.com>
2021-07-03 15:41:35 -0600

    exercise 1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -14,17 +14,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const addGood=nuevoValor =>{
     setGood(nuevoValor)
+    setAll(all+1)
   }
   const addBad=nuevoValor =>{
     setBad(nuevoValor)
+    setAll(all+1)
   }
   const addNeutral=nuevoValor =>{
     setNeutral(nuevoValor)
+    setAll(all+1)
   }
-  
+  const average=()=>{
+    let positive = good
+    let negative = bad*-1
+    return (positive+negative)/all
+  }
+  const positivePercent=()=>{
+    return (good/all)*100
+  }
   return (
     <div>
       <h1>Give feedback</h1>
@@ -39,6 +50,9 @@ const App = () => {
           <h2>good</h2><Display value={good}></Display>
           <h2>neutral</h2><Display value={neutral}></Display>
           <h2>bad</h2><Display value={bad}></Display>
+          <h2>all</h2><Display value={all}></Display>
+          <h2>average</h2><Display value={average()}></Display>
+          <h2>positive</h2><Display value={positivePercent()+"%"}></Display>
         </div>
       </div>
     </div>
